fbfb0ae45d75f0d76a71c6e2334520293de1c089
Михаил Ольховиков <miholxovikov92@mail.ru>
2026-03-23 12:26:46 +0500

    Ревью 2

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -8,7 +8,9 @@ from . import views
 router = DefaultRouter()
 
 router.register(r"tags", views.TagViewSet, basename="tags")
-router.register(r"ingredients", views.IngredientViewSet, basename="ingredients")
+router.register(
+    r"ingredients", views.IngredientViewSet, basename="ingredients"
+)
 router.register(r"recipes", views.RecipeViewSet, basename="recipes")
 urlpatterns = [
     path("", include(router.urls)),
