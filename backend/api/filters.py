bb502d7733cae37cfcac66e16329b8d667b4b854
Михаил Ольховиков <miholxovikov92@mail.ru>
2026-03-26 21:44:02 +0500

    Замечания 1+

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,17 +1,30 @@
-# api/filters.py
+"""
+Модуль фильтров для Django REST Framework.
+
+Содержит кастомные фильтры для моделей Ingredient и Recipe,
+позволяющие фильтровать данные через API с поддержкой поиска,
+тегов, избранного и списка покупок.
+"""
+
 from django_filters import rest_framework as filters
 from recipes.models import Ingredient, Recipe, Tag
 
 
 class IngredientFilter(filters.FilterSet):
+    """Фильтр для модели Ingredient."""
+
     name = filters.CharFilter(lookup_expr='istartswith')
 
     class Meta:
+        """Метаданные фильтра."""
+
         model = Ingredient
         fields = ['name']
 
 
 class RecipeFilter(filters.FilterSet):
+    """Фильтр для модели Recipe."""
+
     tags = filters.ModelMultipleChoiceFilter(
         field_name='tags__slug',
         to_field_name='slug',
@@ -24,15 +37,19 @@ class RecipeFilter(filters.FilterSet):
     author = filters.NumberFilter(field_name='author__id')
 
     def filter_is_favorited(self, queryset, name, value):
+        """Добавляет фильтрацию по избранным рецептам текущего пользователя."""
         if value and self.request.user.is_authenticated:
             return queryset.filter(favorited_by__user=self.request.user)
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
+        """Добавляет фильтрацию по рецептам в списке покупок пользователя."""
         if value and self.request.user.is_authenticated:
             return queryset.filter(shopping_cart__user=self.request.user)
         return queryset
 
     class Meta:
+        """Метаданные фильтра."""
+
         model = Recipe
         fields = ['is_favorited', 'is_in_shopping_cart', 'author', 'tags']
